9915fbd62d7985d67ea4900af9d6e91288e1bbd1
Evelina <evelinasutiene@yahoo.com>
2018-10-08 20:27:09 +0300

    pakeiciau i geresni

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println ("Labas iveskite skaiciu");
         Scanner sc = new Scanner ( System.in );
-        int a = sc.nextInt ();
+        int ivestasskaicius = sc.nextInt ();
         System.out.println (("Iveskiteskaiciu"));
 	// write your code here
     }
