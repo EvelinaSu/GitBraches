27e03716a350499827cd1a2be51b18f583bfe1fa
Evelina <evelinasutiene@yahoo.com>
2018-10-08 20:16:51 +0300

    veikiantis kskaiciu nuskaitymas ir ju isvedimas i ekrana

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println ("Labas iveskite skaiciu");
+        Scanner sc = new Scanner ( System.in );
+        int a = sc.nextInt ();
+        System.out.println (("Iveskiteskaiciu"));
 	// write your code here
     }
 }
